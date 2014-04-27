25a59910d6b10253e1f244b1d4eb404536cf8748
Paul Chen <pochuan@corn04.stanford.edu>
2014-04-27 16:43:20 -0700

    added lda

---
+++ files/classifier/lda.py
@@ -0,0 +1,58 @@
+import numpy
+from sklearn.lda import LDA
+
+# load training data
+train_file = open('../feats/train_formatted.lsvm', 'r')
+lines = train_file.readlines()
+train_file.close()
+
+# convert training data to sklearn format
+X_train_list = []
+Y_train_list = []
+for i, line in enumerate(lines):
+    split = line.split(' ')
+    split = split[:-1]
+    for j, feature in enumerate(split):
+        if j == 0:
+            split[0] = int(split[0])
+        else:
+            split2 = feature.split(':')
+            split[j] = float(split2[1])   
+    Y_train_list.append(split[0])
+    X_train_list.append(split[1:])
+X_train = numpy.array(X_train_list)
+Y_train = numpy.array(Y_train_list)
+
+# load test data
+test_file = open('../feats/test_formatted.lsvm', 'r')
+lines = test_file.readlines()
+test_file.close()
+
+# convert test data to sklearn format
+X_test_list = []
+Y_test_list = []
+for i, line in enumerate(lines):
+    split = line.split(' ')
+    split = split[:-1]
+    for j, feature in enumerate(split):
+        if j == 0:
+            split[0] = int(split[0])
+        else:
+            split2 = feature.split(':')
+            split[j] = float(split2[1]) 
+    #print split
+    Y_test_list.append(split[0])
+    X_test_list.append(split[1:])
+X_test = numpy.array(X_test_list)
+Y_test = numpy.array(Y_test_list)
+#print sum(numpy.isinf(X_train))
+
+
+
+# LDA
+clf = LDA()
+clf.fit(X_train,Y_train)
+qda_pred = clf.predict(X_test)
+accuracy = sum(adq_pred == Y_test)/Y_test.size
+print 'LDA Accuracy: ' + str(accuracy)
+
