5facc7230f8141219018452f5111648f57463a5d
Julia Anna Maria Neidert <jneid@corn20.stanford.edu>
2014-04-27 15:33:15 -0700

    Initial commit

---
+++ files/classifier/svm.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn.svm import SVC
+
+X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+y = np.array([1, 1, 2, 2])
+clf = SVC()
+clf.fit(X, y) 
+print(clf.predict([[-0.8, -1]]))
